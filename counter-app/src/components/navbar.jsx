b63507a88a08709abf57bb1b4e28d18d254cd7ba
Natasha <randomkeyz@gmail.com>
2020-12-10 15:46:00 -0600

    Removing directories with security issues

---
+++ counter-app/src/components/navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+// Stateless Functional Component
+const NavBar = ({ totalCounters }) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <button className="navbar-brand">
+        Navbar{" "}
+        <span className="badge badge-pill badge-secondary">
+          {totalCounters}
+        </span>
+      </button>
+    </nav>
+  );
+};
+
+export default NavBar;
