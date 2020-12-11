7e5081885869d1edf12fae6097511d322a830066
randomkeyz <randomkeyz@gmail.com>
2020-12-11 15:38:01 -0600

    Finishing basics practice

--- vue/basics-01-starting-code/app.js
+++ vue/basics-01-starting-code/app.js
@@ -1,10 +1,21 @@
 const app = Vue.createApp({
   data() {
     return {
-      courseGoal: 'Learn Vue JS',
+      courseGoalA: 'Learn Vue',
+      courseGoalB: 'Learn JS',
       vueLink: 'https://v3.vuejs.org'
     };
   },
+  methods: {
+    outputGoal(){
+      const randomNumber = Math.random();
+      if(randomNumber < 0.5){
+        return this.courseGoalA;
+      } else {
+        return this.courseGoalB;
+      }
+    }
+  }
 });
 
 app.mount("#user-goal");
