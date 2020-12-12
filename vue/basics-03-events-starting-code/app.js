08c33cb49e3e7b68e6456c72ec1970b3b394ddd4
randomkeyz <randomkeyz@gmail.com>
2020-12-11 18:59:33 -0600

    Completed event practice

--- vue/basics-03-events-starting-code/app.js
+++ vue/basics-03-events-starting-code/app.js
@@ -2,8 +2,27 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: "",
+      confirmedName: "",
     };
+  },
+  methods: {
+    confirmName() {
+      this.confirmedName = this.name;
+    },
+    submitForm() {
+      console.log("Submitted!");
+    },
+    setName(event, lastName) {
+      this.name = event.target.value + " " + lastName;
+    },
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    subtract(num) {
+      this.counter = this.counter - num;
+    },
   },
 });
 
-app.mount('#events');
+app.mount("#events");
