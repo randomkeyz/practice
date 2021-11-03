054fb3528a0c9287abd71f160a21db95631e1989
Natasha <randomkeyz@gmail.com>
2021-11-03 14:38:10 -0500

    Adding logic for scoring

--- poke-quiz/app.js
+++ poke-quiz/app.js
@@ -1,17 +1,7 @@
 const form = document.forms.pokequiz;
-const correctAnswers = [
-    "C",
-    "A",
-    "D",
-    "B",
-    "B",
-    "D",
-    "A",
-    "C",
-    "B",
-    "D",
-];
+const correctAnswers = ["C","A","D","B","B","D","A","C","B","D",];
 let userAnswers = [];
+let score = 0;
 
 form.addEventListener('submit', e => {
     // Prevent default submit
@@ -19,11 +9,17 @@ form.addEventListener('submit', e => {
 
     // Store user answers in array
     for(i = 1; i < 11; i++){
-        let q = "q" + i;
-        userAnswers.push(form[q].value);
+        userAnswers.push(form["q" + i].value);
     }
 
     //Compare user answers to correct answers
-    let score = 0;
+    userAnswers.forEach((answer, index) => {
+        if(answer === correctAnswers[index]) score += 10;
+    });
     
+
+    // Update DOM
+    score < 100 ? document.querySelector('#main-title').innerHTML = `You are ${score}% of the way to being a Pokemon Master!` : document.querySelector('#main-title').innerHTML = `Congratulations! You ARE a Pokemon Master!`
+
+    window.scroll(0,0);
 });
