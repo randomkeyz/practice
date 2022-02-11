912fc738813cb93c8c50562e610b55375dd65a67
Natasha <randomkeyz@gmail.com>
2022-02-11 14:34:36 -0600

    Adding audio files and renaming images folder. Adding Projectile class module

---
+++ space-shooter/js/modules/model/projectile.js
@@ -0,0 +1,9 @@
+export default class Projectile{
+    constructor(x, y, color, direction, speed) {
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+        this.color = color;
+        this.direction = direction;
+    }
+}
