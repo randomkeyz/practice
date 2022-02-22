49be84c6f4608094c7782ce60c447e43a430928d
Natasha <randomkeyz@gmail.com>
2022-02-22 11:24:23 -0600

    Adding boundary method. Updating configs. Updating update method to account for enemy projectiles.

--- space-shooter/js/modules/model/projectile.js
+++ space-shooter/js/modules/model/projectile.js
@@ -1,32 +1,59 @@
 import game from './../controller/game.js';
+import renderer from './../view/renderer.js';
+import Boundary from './boundary.js';
 
 export default class Projectile{
     constructor(x, y, color, type) {
         this.x = x;
         this.y = y;
+        this.radiusX = 3;
+        this.radiusY = 8;
+        this.width = this.radiusX / 2;
+        this.height = this.radiusY / 2;
         this.speed = 8;
         this.color = color;
         this.type = type;
     }
 
-    update(index) {
-        this.y -= this.speed;
-
-        // Remove projectile from projectiles array if it falls below view
-        if(this.y <= 0) game.projectiles.splice(index, 1);
+    boundary() {
+        return new Boundary(
+            this.x,
+            this.y,
+            this.width,
+            this.height
+        );
     }
 
-    draw(x, y, context, color, index) {
-        context.beginPath();
+    update(index) {
+        // Redraw projectile
+        //this.draw(this.x, this.y, this.color, index);
+
+        // Either move or remove projectile
+        if(this.type === 'player') {
+            this.y -= this.speed;
+            
+            // Remove projectile from projectiles array if it falls below view
+            if(this.y <= 0) game.projectiles.splice(index, 1);
+        }
+        if(this.type === 'enemy') {
+            this.y += this.speed;
+
+            // Remove projectile from projectiles array if it falls below view
+            if(this.y >= innerHeight) game.projectiles.splice(index, 1);
+        }
+    }
+
+    draw(x, y, color, index) {
+        renderer.context.beginPath();
         // ctx.arc(x, y, radius, startAngle, endAngle [, counterclockwise]);
-        //context.arc(x, y, 5, 0, 2 * Math.PI);
+        //renderer.context.arc(x, y, 5, 0, 2 * Math.PI);
         
         //ctx.ellipse(x, y, radiusX, radiusY, rotation, startAngle, endAngle [, counterclockwise]);
-        context.ellipse(x, y, 3, 8, Math.PI, 0, 2 * Math.PI);
-        context.fillStyle = color;
-        context.filter = "blur(1px)";
-        context.fill();
-        context.closePath();
+        renderer.context.ellipse(x, y, this.radiusX, this.radiusY, Math.PI, 0, 2 * Math.PI);
+        renderer.context.fillStyle = color;
+        renderer.context.filter = "blur(1px)";
+        renderer.context.fill();
+        renderer.context.closePath();
 
         this.update(index);
     }
